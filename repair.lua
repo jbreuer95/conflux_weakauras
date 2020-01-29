bc404c98a773fda2f3323118ce9aaa8da11e19a0
Jelle Breuer <jelle@breuclan.nl>
2020-01-29 03:12:20 +0100

    Create repair.lua

---
+++ repair.lua
@@ -0,0 +1,41 @@
+function() 
+    local active = aura_env.config["active"];
+    if active then
+        local activeday = aura_env.config["day"];    
+        local day = date("*t").wday;
+        if day == activeday then
+            if zoneName == "Molten Core" or zoneName == "Onyxia's Lair" then
+                return false;    
+            else 
+                local i,cur,max;
+                for i=1,20 do
+                    cur,max=GetInventoryItemDurability(i);
+                    if cur and max then
+                        if (cur/max) < 1 then
+                            return true;
+                        end
+                    end
+                end
+                return false;
+            end
+        else 
+            return false;    
+        end
+    else
+        return false;
+    end  
+end
+
+function() 
+    local total = 0;
+    local totalmax = 0;
+    local i,cur,max;
+    for i=1,20 do
+        cur,max=GetInventoryItemDurability(i);
+        if cur and max then            
+            total = total + cur;
+            totalmax = totalmax + max;
+        end
+    end
+    return math.floor(total / totalmax * 100);
+end
