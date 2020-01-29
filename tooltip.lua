3889e84d9bcc3b40186c16e173f8b1d4767372c0
Jelle Breuer <jelle@breuclan.nl>
2020-01-29 03:15:05 +0100

    Update tooltip.lua

--- tooltip.lua
+++ tooltip.lua
@@ -1,19 +1,30 @@
-function MouseoverTooltip(owner, item, amount)    
+function MouseoverTooltip(owner)    
     GameTooltip:SetOwner(owner, "ANCHOR_BOTTOM");
     GameTooltip:SetPoint("TOP", owner, "BOTTOM");
     GameTooltip:ClearLines();
-    GameTooltip:AddLine(item);
-    GameTooltip:AddLine("Needed: " .. amount);
-    GameTooltip:AddLine("In bag: " .. GetItemCount(item));
-    GameTooltip:AddLine("In bank: " .. GetItemCount(item, true) -  GetItemCount(item));
-    GameTooltip:AddLine("Remaining: " .. amount - GetItemCount(item, true));
+    GameTooltip:AddLine("test");
     GameTooltip:Show();
 end
 
-local frame = WeakAuras.regions[id].region
-local item = GetItemInfo(aura_env.config["name"]);
-local amount = aura_env.config["amount"];
+local frame = WeakAuras.GetRegion(aura_env.id, aura_env.cloneId)
 
 frame:EnableMouse(true)
-frame:SetScript("OnEnter", function() MouseoverTooltip(frame, item, amount);end)
+frame:SetScript("OnEnter", function() MouseoverTooltip(frame);end)
 frame:SetScript("OnLeave", WeakAuras.HideTooltip);
+
+
+
+local region = aura_env.regio
+if not region.tooltipFrame then
+    region.tooltipFrame = CreateFrame("frame", nil, region);
+    region.tooltipFrame:SetAllPoints(region);
+    region.tooltipFrame:SetScript("OnEnter", function()
+        GameTooltip:SetOwner(region, "ANCHOR_BOTTOM");
+        GameTooltip:SetPoint("TOP", region, "BOTTOM");
+        GameTooltip:ClearLines();
+        GameTooltip:AddLine("test");
+        GameTooltip:Show();
+    end);
+    region.tooltipFrame:SetScript("OnLeave", WeakAuras.HideTooltip);
+end
+region.tooltipFrame:EnableMouse(true);
