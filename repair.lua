d48a210ac1eec069cf04ccec087079837e849861
Jelle Breuer <jelle@breuclan.nl>
2020-01-29 05:59:55 +0100

    Update repair.lua

--- repair.lua
+++ repair.lua
@@ -1,42 +1,34 @@
 -- PLAYER_DEAD, UPDATE_INVENTORY_DURABILITY, PLAYER_ENTERING_WORLD, ZONE_CHANGED, ZONE_CHANGED_INDOORS, ZONE_CHANGED_NEW_AREA
-function() 
-    local active = aura_env.config["active"];
-    if active then
-        local activeday = aura_env.config["day"];    
-        local day = date("*t").wday;
-        if day == activeday then
-            if zoneName == "Molten Core" or zoneName == "Onyxia's Lair" then
-                return false;    
-            else 
-                local i,cur,max;
-                for i=1,20 do
-                    cur,max=GetInventoryItemDurability(i);
-                    if cur and max then
-                        if (cur/max) < 1 then
-                            return true;
-                        end
-                    end
-                end
-                return false;
-            end
-        else 
-            return false;    
-        end
-    else
-        return false;
-    end  
-end
+function()
+    local testmode = aura_env.config["testmode"]
 
-function() 
-    local total = 0;
-    local totalmax = 0;
+    if testmode ~= true then
+        local ginfo = GetGuildInfoText()
+        local raid = aura_env.config["raid"]
+        if string.match(ginfo, raid) then
+            aura_env.raidday = true
+        else
+            aura_env.raidday = false
+        end
+
+        local zoneName = GetRealZoneText()
+        if zoneName == raid then
+            aura_env.raided = true
+        end
+
+        if aura_env.raided == true or aura_env.raidday == false then
+            return false
+        end
+    end
+
     local i,cur,max;
     for i=1,20 do
         cur,max=GetInventoryItemDurability(i);
-        if cur and max then            
-            total = total + cur;
-            totalmax = totalmax + max;
+        if cur and max then
+            if (cur/max) < 1 then
+                return true;
+            end
         end
     end
-    return math.floor(total / totalmax * 100);
+    return false;
 end
