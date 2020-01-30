11fe97fe98d82c58359ef4bf82b8e8ecc4a26e6c
Jelle Breuer <jelle@breuclan.nl>
2020-01-30 01:24:02 +0100

    Update repair.lua

--- repair.lua
+++ repair.lua
@@ -1,14 +1,22 @@
 -- PLAYER_DEAD, UPDATE_INVENTORY_DURABILITY, PLAYER_ENTERING_WORLD, ZONE_CHANGED, ZONE_CHANGED_INDOORS, ZONE_CHANGED_NEW_AREA
 function()
-    local testmode = aura_env.config["testmode"]
+    local testmode = aura_env.config.testmode
 
     if testmode ~= true then
+        local raidday = false
         local ginfo = GetGuildInfoText()
-        local raid = aura_env.config["raid"]
-        if string.match(ginfo, raid) then
-            aura_env.raidday = true
-        else
-            aura_env.raidday = false
+
+        local raids = aura_env.config.raids
+        for raid in raids:gmatch("([^,]+)") do
+            local _, i = string.find(ginfo, raid)
+            if i then
+                local day = date("*t").wday
+                i = i + 2
+                local rday = tonumber(ginfo:sub(i,i))
+                if  rday == day then
+                    raidday = true
+                end
+            end
         end
 
         local zoneName = GetRealZoneText()
@@ -16,7 +24,7 @@ function()
             aura_env.raided = true
         end
 
-        if aura_env.raided == true or aura_env.raidday == false then
+        if aura_env.raided == true or raidday == false then
             return false
         end
     end
